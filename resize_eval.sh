4da2f39d233ff1a3022c0ebb3e3b25159f7dd0ed
robot.ai.network <robot.ai.network@outlook.com>
2020-07-01 10:53:10 +0800

    add resize shell

---
+++ resize_eval.sh
@@ -0,0 +1,1 @@
+python image_resizer_imagent.py -i ~/images/ILSVRC2015/Data/CLS-LOC/val -o ~/data/val -s 32 -a box
---
+++ resize_train.sh
@@ -0,0 +1,1 @@
+python3 image_resizer_imagent.py -i ~/images/ILSVRC2015/Data/CLS-LOC/train -o ~/data/ -s 32 -a box -r -j 10 
