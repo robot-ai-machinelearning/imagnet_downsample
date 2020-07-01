1002b81353704854afd3fd257a49b6f916560ae1
robot.ai.network <robot.ai.network@outlook.com>
2020-07-01 10:31:47 +0800

    add resize_img_folder

--- resizer.py
+++ resizer.py
@@ -41,3 +41,36 @@ def str2alg(str):
     str = str.lower()
     return alg_dict.get(str, None)
 
+# Takes in_dir, out_dir and alg as strings
+# resize images from in_dir using algorithm deduced from
+# alg string and puts them to "out_dir/alg/" folder
+def resize_img_folder(in_dir, out_dir, alg):
+    print('Folder %s' % in_dir)
+
+    alg_val = str2alg(alg)
+
+    if alg_val is None:
+        print("Sorry but this algorithm (%s) is not available, use help for more info." % alg)
+        return
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    for filename in os.listdir(in_dir):
+        # Exception raised when file is not an image
+        try:
+            im = Image.open(os.path.join(in_dir, filename))
+
+            # Convert grayscale images into 3 channels
+            if im.mode != "RGB":
+                im = im.convert(mode="RGB")
+
+            im_resized = im.resize((size, size), alg_val)
+            # Get rid of extension (.jpg or other)
+            filename = os.path.splitext(filename)[0]
+            im_resized.save(os.path.join(out_dir, filename + '.png'))
+        except OSError as err:
+            print("This file couldn't be read as an image")
+            with open("log.txt", "a") as f:
+                f.write("Couldn't resize: %s" % os.path.join(in_dir, filename))
+
