b53379e8c929060192f6c87dfd5627d52af9ef8d
robot.ai.network <robot.ai.network@outlook.com>
2020-07-02 11:26:34 +0800

    update necessary txt

--- resize_train.sh
+++ resize_train.sh
@@ -1,1 +1,1 @@
-python3 image_resizer_imagent.py -i ~/images/ILSVRC2015/Data/CLS-LOC/train -o ~/data/ -s 32 -a box -r -j 10 
+python3 resizer.py -i ~/images/ILSVRC2015/Data/CLS-LOC/train -o ~/data/ -s 32 -a box -r -j 2 
